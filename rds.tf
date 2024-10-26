47fd2c69d16479102d3dc9d887f4a5bdc192d9c8
saurabh <saurabhtiwari2811@gmail.com>
2024-10-26 16:38:59 +0530

    initial code

--- rds.tf
+++ rds.tf
@@ -2,7 +2,7 @@ resource "aws_db_instance" "default" {
   allocated_storage    = 5
   storage_type         = "gp2"
   engine               = "mysql"
-  engine_version       = "5.7"
+  engine_version       = "5.7.40"
   instance_class       = "db.t2.micro"
   db_name              = "mydb"
   username             = "foo"
